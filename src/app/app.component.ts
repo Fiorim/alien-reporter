caf07a785b13c35b24d4c7f510e38ac7b30cae32
Eric Fiorim <ericfiorim@Erics-MacBook-Pro.local>
2018-12-10 14:06:12 +0000

    list style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,25 +16,13 @@ export class AppComponent implements OnInit {
 
   constructor(private alienFormService: ReportFormService) { }
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi'
-  ];
-
-  drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
-  }
-
   ngOnInit() {
     this.alienFormService.getForm().subscribe((response: AlienForm) => {
       this.alienForm = response;
     });
+  }
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.alienForm.form, event.previousIndex, event.currentIndex);
   }
 }
 
